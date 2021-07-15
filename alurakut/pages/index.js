2f5ebd23d8c9be84208831895b6405a2b72c8e14
Pedro Berbel <pedroberbel.br@gmail.com>
2021-07-15 14:28:14 -0300

    Added 'amigos' profile link to github profiles

--- alurakut/pages/index.js
+++ alurakut/pages/index.js
@@ -1,6 +1,7 @@
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
-import { AlurakutMenu } from '../src/lib/AlurakutCommons'
+import { AlurakutMenu, OrkutNostalgicIconSet } from '../src/lib/AlurakutCommons'
+import { ProfileRelationsBoxWrapper } from '../src/components/ProfileRelations'
 
 function ProfileSidebar(properties){
   return (
@@ -12,7 +13,7 @@ function ProfileSidebar(properties){
 
 export default function Home() {
   const githubUser = 'pedroberbel';
-
+  const pessoasFavoritas = ['pedroberbel','pedroberbel','pedroberbel','pedroberbel','pedroberbel','pedroberbel'];
   return (
     <div>
       <AlurakutMenu />
@@ -23,11 +24,37 @@ export default function Home() {
         
 
         <div className="welcomeArea" style={{gridArea: 'welcomeArea'}}>
-          <Box>Bem Vindo</Box>
+          <Box>
+            <h1 className="tittle">
+              Bem Vindo(a)
+            </h1>
+
+            <OrkutNostalgicIconSet />
+          </Box>
         </div>
 
         <div className="profileRelationsArea" style={{gridArea: 'profileRelationsArea'}}>
-          <Box>Amigos</Box>
+          <ProfileRelationsBoxWrapper>
+            <h2 className="smallTitle">
+            Amigos  ({pessoasFavoritas.length})
+            </h2>
+            <ul>
+              {pessoasFavoritas.map((itemAtual) => {
+                return (
+                  <li>
+                    <a href={`/users/${itemAtual}`} key={itemAtual}>
+                      <img src={`https://github.com/${itemAtual}.png`} />
+                      <span>{itemAtual}</span>
+                    </a>
+                  </li>
+                )
+              })}
+            </ul>
+
+          </ProfileRelationsBoxWrapper>
+
+
+
           <Box>Comunidades</Box>
         </div>
         
