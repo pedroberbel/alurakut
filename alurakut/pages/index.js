d7b94c224bbc384352e06711d79a82f231b66a00
Pedro Berbel <pedroberbel.br@gmail.com>
2021-07-15 13:38:15 -0300

    Added lib AlutakutCommmons styles and menu | added menu to Home return

--- alurakut/pages/index.js
+++ alurakut/pages/index.js
@@ -1,6 +1,6 @@
 import MainGrid from '../src/components/MainGrid'
 import Box from '../src/components/Box'
-
+import { AlurakutMenu } from '../src/lib/AlurakutCommons'
 
 function ProfileSidebar(properties){
   return (
@@ -14,23 +14,26 @@ export default function Home() {
   const githubUser = 'pedroberbel';
 
   return (
-    <MainGrid>
-      <div className="profileArea" style={{gridArea: 'profileArea'}}>
-        <ProfileSidebar githubUser={githubUser}/>
-      </div>
-      
+    <div>
+      <AlurakutMenu />
+      <MainGrid>
+        <div className="profileArea" style={{gridArea: 'profileArea'}}>
+          <ProfileSidebar githubUser={githubUser}/>
+        </div>
+        
 
-      <div className="welcomeArea" style={{gridArea: 'welcomeArea'}}>
-        <Box>Bem Vindo</Box>
-      </div>
+        <div className="welcomeArea" style={{gridArea: 'welcomeArea'}}>
+          <Box>Bem Vindo</Box>
+        </div>
 
-      <div className="profileRelationsArea" style={{gridArea: 'profileRelationsArea'}}>
-        <Box>Amigos</Box>
-        <Box>Comunidades</Box>
-      </div>
-      
-      
+        <div className="profileRelationsArea" style={{gridArea: 'profileRelationsArea'}}>
+          <Box>Amigos</Box>
+          <Box>Comunidades</Box>
+        </div>
+        
+        
 
-    </MainGrid>
+      </MainGrid>
+    </div>
   )
 }
