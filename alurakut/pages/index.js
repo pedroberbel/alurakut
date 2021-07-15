153ad6fc43842c4a9d2cf7b9e07fbfc41e96bf6a
Pedro Berbel <pedroberbel.br@gmail.com>
2021-07-15 12:26:00 -0300

    Added some css

--- alurakut/pages/index.js
+++ alurakut/pages/index.js
@@ -9,13 +9,48 @@ const Box = styled.div `
   background: #ffffff;
   border-radius: 8px;
 `;
+
+const MainGrid = styled.main `
+  width: 100%;
+  grid-gap: 10px; 
+  margin-left: auto;
+  margin-right: auto;
+  max-width: 500px;
+  padding: 16px;
+  .profileArea{
+    display: none;
+    @media(min-width:860px){
+      display: block;
+    }
+  }
+  @media(min-width: 860px){
+    max-width: 1110px;
+    display: grid;
+    grid-template-areas: "profileArea welcomeArea profileRelationsArea";
+    grid-template-columns: 160px 1fr 312px;
+  }
+`;
 export default function Home() {
   return (
-    <main>
-      <Box>Imagem</Box>
-      <Box>Bem Vindo</Box>
-      <Box>Comunidades</Box>
+    <MainGrid>
+      <div className="profileArea" style={{gridArea: 'profileArea'}}>
+        <Box>
+          <img src="https://github.com/pedroberbel.png"></img>
+        </Box>
+      </div>
+      
 
-    </main>
+      <div className="welcomeArea" style={{gridArea: 'welcomeArea'}}>
+        <Box>Bem Vindo</Box>
+      </div>
+
+      <div className="profileRelationsArea" style={{gridArea: 'profileRelationsArea'}}>
+        <Box>Amigos</Box>
+        <Box>Comunidades</Box>
+      </div>
+      
+      
+
+    </MainGrid>
   )
 }
